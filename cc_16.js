25961e8a2bd9e6f96b197e99e5baa4b007cf9ceb
Amanda Robles <amandarobles@usf.edu>
2025-03-29 14:16:59 -0400

    Task 3 - Add fetchProductsAsync using async/await and try/catch

--- cc_16.js
+++ cc_16.js
@@ -1,7 +1,12 @@
 // Task 2: Fetch Products with .then()
 function fetchProductsThen() { //creating function 
     fetch("https://www.course-api.com/javascript-store-products") //sourcing product data
-    .then(response => response.json())
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+    })
     .then(data => {
         data.forEach(product => {
             console.log(product.fields.name); //logging product name to console
@@ -10,4 +15,18 @@ function fetchProductsThen() { //creating function
     .catch(error => {
         console.error("Fetch error:", error); //logging errors
     });
+}
+
+// Task 3: Fetch Products with async/await
+async function fetchProductsAsync() { //creating function
+    try {
+        const response = await fetch("https://www.course-api.com/javascript-store-products"); //fetching product data
+        if (!response.ok) {
+            throw new Error (`HTTP error! status: ${response.status}`);
+        } 
+        const data = await response.json();
+        displayProducts(data);  //rendering products to page
+    } catch (error) {
+        handleError(error); //passed if error occurs
+    }
 }
